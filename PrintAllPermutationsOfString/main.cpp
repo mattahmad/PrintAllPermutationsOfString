1d749fd14ce215e95dee6ecbf1f0ec685eb20e4e
matt.a@myplanet.com <matt.a@myplanet.com>
2022-10-15 16:57:29 -0400

    Recursion for permutations

--- PrintAllPermutationsOfString/main.cpp
+++ PrintAllPermutationsOfString/main.cpp
@@ -1,14 +1,33 @@
-//
-//  main.cpp
-//  PrintAllPermutationsOfString
-//
-//  Created by Matt Ahmad on 2022-10-15.
-//
-
+#include <string>
 #include <iostream>
 
+using namespace std;
+
+void string_permutation( std::string& orig, std::string& perm )
+{
+    
+    if( orig.empty() )
+    {
+        std::cout<<perm<<std::endl;
+        return;
+    }
+    
+    for(int i=0;i<orig.size();++i)
+    {
+        std::string orig2 = orig;
+        orig2.erase(i,1);
+        std::string perm2 = perm;
+        perm2 += orig.at(i);
+        string_permutation(orig2,perm2);
+    }
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    std::string orig="ABCDE";
+    std::string perm;
+    string_permutation(orig,perm);
+    cout << "Complete!"<<endl;
+    system("pause");
     return 0;
 }
